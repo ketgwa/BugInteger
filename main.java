eee7189866d265bec665001374972d9b924a6b79
ketgwa <45768231+ketgwa@users.noreply.github.com>
2020-12-17 22:12:58 +0100

    Update main.java

--- main.java
+++ main.java
@@ -3,25 +3,25 @@ package eserciziobuginteger;
 import java.math.BigInteger;
 
 /**
- * @version 0.6
+ * @version 1.0
+ * @author Sakurti Abdel
+ * @author Mariottini Matteo
+ * @author Gregorelli Michele
+ * @author Palestro Lorenzo
  */
 
 public class EsercizioBugInteger {
     public static void main(String[] args) {
         try {
-            /*
-            realizzare un file di testo contente tanti numeri elencati, il main recupererà i numeri, due a due
-            e farà tutte le operazioni possibili tra di essi, confrontando i risultati con quelli fatti con la classe BigInteger
-            */
-            int numeri = 24, dim = 1000;
+            int numeri = 25, dim = 1000;
             String str1[] = {"78", "23", "-91", "99", "-24", "-788",
                 "8436", "43", "11", "-050", "12766435245667", "-3246576",
                 "-156", "-400", "0", "0", "8769", "294567936349679202620967349486987527896",
-                "154352", "1", "13315", "090090", "010", "12532"};
+                "154352", "1", "13315", "090090", "010", "12532", "7700"};
             String str2[] = {"675", "-9", "1352", "-0975", "4", "1", 
                 "-876345", "3243677", "11", "65768", "34879", "-5657687898978651235678363",
                 "-156", "-300", "-0", "0", "8769", "21352463576879687462354657886543253645",
-                "124", "-3246357464", "123524", "-123523", "153", "-987218545"};
+                "124", "-3246357464", "123524", "-123523", "153", "-987218545", "90"};
             int esponenti[] = {10, 4, 6, 9, 13};
             BigInteger big1[], big2[];
             BugInteger bug1[], bug2[];
@@ -144,7 +144,7 @@ public class EsercizioBugInteger {
                     s2[i] = "0";
                 }
                 else
-                    s2[i] = big1[i].divide(big2[i]).toString();
+                    s2[i] = bug1[i].divide(bug2[i]).toString();
             }
             System.out.println("div bug: " + (System.nanoTime() - inizio)/1E9);
             for (int i=0; i<numeri; i++) {
@@ -172,7 +172,7 @@ public class EsercizioBugInteger {
                     s2[i] = "0";
                 }
                 else
-                    s2[i] = big1[i].remainder(big2[i]).toString();
+                    s2[i] = bug1[i].remainder(bug2[i]).toString();
                 //System.out.println("s2["+i+"]="+s2[i]);
             }
             System.out.println("mod bug: " + (System.nanoTime() - inizio)/1E9);
@@ -257,3 +257,4 @@ public class EsercizioBugInteger {
         }
     }
 }
+
